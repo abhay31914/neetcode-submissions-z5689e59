a492d2b557d8e458b928a38800b6c2d7752eb84a
abhay31914 <43891708+abhay31914@users.noreply.github.com>
2026-04-12 23:15:12 +0530

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& V, int val) {
+        int l = 0;
+    int r = V.size()-1;
+
+    if(val > V[r]) return r+1;
+
+    while(l<r){
+        int mid = l + (r-l)/2;
+
+        if(V[mid] < val){
+            l = mid+1;
+        }
+        else{
+            r = mid;
+        }
+    }
+    return l;
+        
+    }
+};
