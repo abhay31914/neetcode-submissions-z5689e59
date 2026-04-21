2c7e675e0146659de0ce0385cd50a9e5b34b9c0c
abhay31914 <43891708+abhay31914@users.noreply.github.com>
2026-04-21 11:00:18 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,29 @@
+class KthLargest {
+public:
+priority_queue<int, vector<int>, greater<int>> pq;
+int n;
+
+    KthLargest(int k, vector<int>& nums): n(k) {
+
+        for(int x: nums){
+            pq.push(x);
+
+            if(pq.size() > n){
+                pq.pop();
+            }
+        }
+        
+    }
+    
+    int add(int val) {
+
+        pq.push(val);
+
+        if(pq.size()> n){
+            pq.pop();
+        }
+
+        return pq.top();
+        
+    }
+};
