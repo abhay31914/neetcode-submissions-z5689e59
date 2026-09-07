185aa30d5e2b998c5bb87ce7a070d4a26c2e7c78
abhay31914 <43891708+abhay31914@users.noreply.github.com>
2026-09-07 23:10:29 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+
+    bool isPalindrome(string s) {
+
+        int i = 0;
+        int j = s.size()-1;
+
+        while(i<j){
+
+            if(isalnum(s[i]) && isalnum(s[j])){
+                if(tolower(s[i]) != tolower(s[j])) return false;
+                i++;
+                j--;
+            }
+
+            else if(!isalnum(s[i])) i++;
+
+            else j--;
+        }
+        return true;
+    }
+};
