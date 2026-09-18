28a5f1fceb6a63a5500637a9ce4b93bd5b0819c4
abhay31914 <43891708+abhay31914@users.noreply.github.com>
2026-09-18 14:25:31 +0530

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> pq;
+
+        for(int x: stones){
+            pq.push(x);
+        }
+
+        while(pq.size() > 1){
+
+            int s1 = pq.top();
+            pq.pop();
+            int s2 = pq.top();
+            pq.pop();
+
+            if(s1 != s2) pq.push(s1-s2);
+
+        }
+        return pq.size() == 1 ? pq.top(): 0;
+        
+    }
+};
